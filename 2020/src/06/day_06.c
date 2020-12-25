74dabf6e1a918bf1055636980a85a6865566257c
Pascal Guenther <pascal.develop@pguenther.net>
2020-12-25 18:13:46 +0100

    Solution Day 06 Part 2

--- 2020/src/06/day_06.c
+++ 2020/src/06/day_06.c
@@ -1,8 +1,6 @@
 #if defined(_MSC_VER)
 #define _CRT_SECURE_NO_DEPRECATE
 #include <intrin.h>
-#elif defined(__GNUC__) || defined(__GNUG__)
-#else
 #endif
 #include <inttypes.h>
 #include <stdbool.h>
@@ -10,17 +8,23 @@
 #include <stdio.h>
 #include <string.h>
 
-#define AOC_INPUT_MAX_LETTERS    (26)
-#define AOC_INPUT_LINEBUF_SIZE   (AOC_INPUT_MAX_LETTERS + sizeof((char)'\n') + sizeof((char)'\0'))
-#define AOC_MAX_PESSENGER_GROUPS (1024)
+#define AOC_INPUT_MAX_LETTERS  (26)
+#define AOC_INPUT_LINEBUF_SIZE (AOC_INPUT_MAX_LETTERS + sizeof((char)'\n') + sizeof((char)'\0'))
 
-static bool AddLine(const char *pLine, uint32_t *const pGroupDeclaration)
+typedef struct
 {
-    if ((NULL == pLine) || (NULL == pGroupDeclaration))
+    uint32_t anyoneYes;
+    uint32_t everyoneYes;
+} GroupDeclaration_t;
+
+static bool ParseSingleDeclaration(const char *pLine, uint32_t *const pDeclaration)
+{
+    if ((NULL == pLine) || (NULL == pDeclaration))
     {
         return false;
     }
 
+    *pDeclaration = 0u;
     for (size_t i = 0u; i < AOC_INPUT_MAX_LETTERS; ++i)
     {
         const char c = pLine[i];
@@ -42,7 +46,7 @@ static bool AddLine(const char *pLine, uint32_t *const pGroupDeclaration)
         }
 
         const uint8_t answerIndex = pLine[i] - 'a';
-        *pGroupDeclaration |= 1u << answerIndex;
+        *pDeclaration |= 1ul << answerIndex;
     }
     return true;
 }
@@ -66,69 +70,84 @@ static uint32_t CountAnswerYes(const uint32_t declaration)
 #endif
 }
 
-static bool LoadCustomsDeclarationsFromFile(const char *const pFilename, const size_t groupDeclarationsSize,
-                                            uint32_t *const pGroupDeclarations)
+static bool LoadCustomsDeclarationsFromFile(const char *const pFilename, GroupDeclaration_t *const pSumOfGroupDeclarations)
 {
-    if ((NULL == pFilename) || (0u == groupDeclarationsSize) || (NULL == pGroupDeclarations))
+    if ((NULL == pFilename) || (NULL == pSumOfGroupDeclarations))
     {
         return false;
     }
-    memset(pGroupDeclarations, 0u, groupDeclarationsSize * sizeof(*pGroupDeclarations));
 
-    FILE *pFile = fopen(pFilename, "r");
+    FILE *const pFile = fopen(pFilename, "r");
     if (NULL == pFile)
     {
-        puts("Failed to open input file\n");
+        puts("Failed to open input file");
         return false;
     }
 
-    char aLineBuf[AOC_INPUT_LINEBUF_SIZE];
-    size_t groupIndex = 0u;
-
-    while (fgets(aLineBuf, sizeof(aLineBuf), pFile))
+    bool endOfFile = false;
+    do
     {
-        if (groupIndex >= groupDeclarationsSize)
+        GroupDeclaration_t declarationsOfGroup = {
+            .anyoneYes = 0u,
+            // set flag to "yes" for all possible answers, will be reset if anyone answers "no"
+            .everyoneYes = (1ul << AOC_INPUT_MAX_LETTERS) - 1u,
+        };
+        while (true)
         {
-            puts("Too many passenger groups");
-            fclose(pFile);
-            return 1;
+            char aLineBuf[AOC_INPUT_LINEBUF_SIZE] = {0u};
+            if (!fgets(aLineBuf, sizeof(aLineBuf), pFile))
+            {
+                endOfFile = true;
+                break;
+            }
+
+            if (aLineBuf[0] == '\n')
+            {
+                break;
+            }
+
+            uint32_t individualDeclaration = 0u;
+            if (!ParseSingleDeclaration(&aLineBuf[0u], &individualDeclaration))
+            {
+                fclose(pFile);
+                return false;
+            }
+            // Part 1
+            declarationsOfGroup.anyoneYes |= individualDeclaration;
+            // Part 2
+            // reset the group's bits corresponding to the questions the person didn't answer "yes"
+            declarationsOfGroup.everyoneYes &= individualDeclaration;
         }
-        if (aLineBuf[0] == '\n')
-        {
-            groupIndex++;
-            continue;
-        }
-        AddLine(&aLineBuf[0u], &pGroupDeclarations[groupIndex]);
-    }
+        pSumOfGroupDeclarations->anyoneYes += CountAnswerYes(declarationsOfGroup.anyoneYes);
+        pSumOfGroupDeclarations->everyoneYes += CountAnswerYes(declarationsOfGroup.everyoneYes);
+    } while (!endOfFile);
     fclose(pFile);
     return true;
 }
 
 int main(const int argc, const char *argv[])
 {
-    puts("==Day 06==\n");
+    puts("==Day 06==");
     if (argc < 2)
     {
-        puts("Please specify an input file\n");
+        puts("Please specify an input file");
         return 1;
     }
 
-    uint32_t aGroupDeclarations[AOC_MAX_PESSENGER_GROUPS] = {0u};
-    if (!LoadCustomsDeclarationsFromFile(argv[1u], (sizeof(aGroupDeclarations) / sizeof(*aGroupDeclarations)),
-                                         &aGroupDeclarations[0u]))
+    GroupDeclaration_t sumsOfAnswerYesInGroupDeclarations = {0u};
+    if (!LoadCustomsDeclarationsFromFile(argv[1u], &sumsOfAnswerYesInGroupDeclarations))
     {
-        puts("Failed to open or parse input file\n");
+        puts("Failed to open or parse input file");
         return 1;
     }
 
     {
-        puts("=Part 1=\n");
-        uint32_t sumOfGroupCounts = 0u;
-        for (size_t i = 0u; i < (sizeof(aGroupDeclarations) / sizeof(*aGroupDeclarations)); ++i)
-        {
-            sumOfGroupCounts += CountAnswerYes(aGroupDeclarations[i]);
-        }
-        printf("Sum of counts: %" PRIu32 "\n", sumOfGroupCounts);
+        puts("=Part 1=");
+        printf("Sum of \"anybody in group answered yes\" counts: %" PRIu32 "\n", sumsOfAnswerYesInGroupDeclarations.anyoneYes);
+    }
+    {
+        puts("=Part 2=");
+        printf("Sum of \"everyone in group answered yes\" counts: %" PRIu32 "\n", sumsOfAnswerYesInGroupDeclarations.everyoneYes);
     }
 
     return 0;
