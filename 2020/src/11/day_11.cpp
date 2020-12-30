141fdb407d469256fd58f8bb4717621641b65f04
Pascal Guenther <pascal.develop@pguenther.net>
2020-12-30 02:04:08 +0100

    Solution Day 11 Part 2

--- 2020/src/11/day_11.cpp
+++ 2020/src/11/day_11.cpp
@@ -11,12 +11,12 @@
 #include <variant>
 #include <vector>
 
-
 #include "fileUtils.hpp"
 
 namespace
 {
 
+using std::int8_t;
 using std::size_t;
 
 constexpr std::string_view exampleInput[]{
@@ -37,9 +37,89 @@ struct Grid
     size_t width{0u};
     std::vector<GridElement> elems;
 
-    bool RunSimulationStep()
+    enum class SearchMode
     {
-        const auto prevElems = this->elems;
+        adjacent,
+        firstInAnyDirection,
+    };
+
+    static inline bool IsNextSeatInDirectionOccupied(const Grid &searchGrid, const size_t xPos, const size_t yPos,
+                                                     const int8_t xDirection, const int8_t yDirection)
+    {
+        const auto witdth_ = searchGrid.width;
+        const auto height_ = searchGrid.elems.size() / witdth_;
+        for (auto x = xPos + xDirection, y = yPos + yDirection; ((x < witdth_) && (y < height_));
+             x += xDirection, y += yDirection)
+        {
+            static_assert(std::is_unsigned<decltype(x)>::value, "");
+            static_assert(std::is_unsigned<decltype(y)>::value, "");
+            static_assert(std::is_same<std::decay<decltype(yPos)>::type, decltype(y)>::value, "");
+            static_assert(std::is_same<std::decay<decltype(yPos)>::type, decltype(y)>::value, "");
+            const auto offset = y * witdth_;
+            switch (searchGrid.elems[offset + x])
+            {
+
+                case GridElement::occupiedSeat:
+                    return true;
+                case GridElement::emptySeat:
+                    return false;
+                default:
+                    continue;
+            }
+        }
+        return false;
+    }
+
+    static inline size_t CountNextOccupiedSeatsInAllDirections(const Grid &searchGrid, size_t xPos, size_t yPos)
+    {
+        size_t foundElements{0u};
+        for (int8_t xDir = -1; xDir <= 1; xDir++)
+        {
+            for (int8_t yDir = -1; yDir <= 1; yDir++)
+            {
+                if ((xDir == 0) && (yDir == 0))
+                {
+                    continue;
+                }
+                if (IsNextSeatInDirectionOccupied(searchGrid, xPos, yPos, xDir, yDir))
+                {
+                    foundElements++;
+                }
+            }
+        }
+        return foundElements;
+    }
+
+    static inline size_t CountOccupiedSeatsAdjacent(const Grid &searchGrid, size_t xPos, size_t yPos)
+    {
+        const auto width_ = searchGrid.width;
+        const auto height_ = searchGrid.elems.size() / searchGrid.width;
+        const auto searchRowBegin = (yPos > 0) ? (yPos - 1) : 0;
+        const auto searchRowEnd = (yPos + 2 > height_) ? height_ : (yPos + 2);
+        const auto searchColumnBegin = (xPos > 0) ? (xPos - 1) : 0;
+        const auto searchColumnEnd = (xPos + 2 > width_) ? width_ : (xPos + 2);
+        size_t foundElements{0u};
+        for (auto searchRow = searchRowBegin; searchRow < searchRowEnd; searchRow++)
+        {
+            for (auto searchColumn = searchColumnBegin; searchColumn < searchColumnEnd; searchColumn++)
+            {
+                if ((yPos == searchRow) && (xPos == searchColumn))
+                {
+                    continue;
+                }
+                const auto pos = (width_ * searchRow) + searchColumn;
+                if (searchGrid.elems[pos] == GridElement::occupiedSeat)
+                {
+                    foundElements++;
+                }
+            }
+        }
+        return foundElements;
+    }
+
+    bool RunSimulationStep(const size_t occupancyThreshold, const SearchMode searchMode)
+    {
+        const auto previous = *this;
         const size_t height = elems.size() / width;
         assert(height * width == elems.size());
         for (size_t y = 0; y < height; y++)
@@ -47,7 +127,7 @@ struct Grid
             for (size_t x = 0; x < width; x++)
             {
                 const auto offset = y * width;
-                const auto prevState = prevElems[offset + x];
+                const auto prevState = previous.elems[offset + x];
                 switch (prevState)
                 {
                     case GridElement::emptySeat:
@@ -57,43 +137,28 @@ struct Grid
                     default:
                         continue;
                 }
-                const auto searchRowBegin = (y > 0) ? (y - 1) : 0;
-                const auto searchRowEnd = (y + 2 > height) ? height : (y + 2);
-                const auto searchColumnBegin = (x > 0) ? (x - 1) : 0;
-                const auto searchColumnEnd = (x + 2 > width) ? width : (x + 2);
-                size_t occupiedNeighborsCnt{0u};
-                for (auto searchRow = searchRowBegin; searchRow < searchRowEnd; searchRow++)
-                {
-                    for (auto searchColumn = searchColumnBegin; searchColumn < searchColumnEnd; searchColumn++)
-                    {
-                        if ((y == searchRow) && (x == searchColumn))
-                        {
-                            continue;
-                        }
-                        const auto pos = (width * searchRow) + searchColumn;
-                        if (GridElement::occupiedSeat == prevElems[pos])
-                        {
-                            occupiedNeighborsCnt++;
-                        }
-                    }
-                }
+
+                const auto occupiedNeighboringSeats = (searchMode == SearchMode::adjacent)
+                                                          ? CountOccupiedSeatsAdjacent(previous, x, y)
+                                                          : CountNextOccupiedSeatsInAllDirections(previous, x, y);
+
                 if (prevState == GridElement::emptySeat)
                 {
-                    if (occupiedNeighborsCnt == 0)
+                    if (occupiedNeighboringSeats == 0)
                     {
                         elems[offset + x] = GridElement::occupiedSeat;
                     }
                 }
                 else if (prevState == GridElement::occupiedSeat)
                 {
-                    if (occupiedNeighborsCnt >= 4)
+                    if (occupiedNeighboringSeats >= occupancyThreshold)
                     {
                         elems[offset + x] = GridElement::emptySeat;
                     }
                 }
             }
         }
-        return (this->elems != prevElems);
+        return (this->elems != previous.elems);
     }
 
     [[nodiscard]] unsigned int CountSeatsOfState(const GridElement state) const
@@ -120,24 +185,39 @@ int Day11_Part1(const Grid &input)
 {
     std::cout << "=Part 1=\n";
     Grid inputCopy{input};
+    constexpr size_t occupancyTreshold{4u};
+    constexpr Grid::SearchMode countAdjacent{Grid::SearchMode::adjacent};
     do
     {
 #if !defined(NDEBUG)
-        // inputCopy.Print();
         static int iteration{0u};
-        std::cout << "Iteation " << ++iteration << "\n";
+        std::cout << "Iteration " << ++iteration << "\n";
+        // inputCopy.Print();
 #endif
-    } while (inputCopy.RunSimulationStep());
+    } while (inputCopy.RunSimulationStep(occupancyTreshold, countAdjacent));
     const auto occupiedCnt = inputCopy.CountSeatsOfState(GridElement::occupiedSeat);
-    std::cout << "In the stable state, " << occupiedCnt << " seats are occupied\n";
+    std::cout << "In the stable state, " << occupiedCnt << " seats are occupied.\n";
 
     return 0;
 }
 
-int Day11_Part2(const Grid & /* input */)
+int Day11_Part2(const Grid &input)
 {
     std::cout << "=Part 2=\n";
-    std::cout << "To be implemented...\n";
+    Grid inputCopy{input};
+    constexpr size_t occupancyTreshold{5u};
+    constexpr Grid::SearchMode countFirstSeatInAnyDirection{Grid::SearchMode::firstInAnyDirection};
+    do
+    {
+#if !defined(NDEBUG)
+        static int iteration{0u};
+        std::cout << "Iteration " << ++iteration << "\n";
+        // inputCopy.Print();
+#endif
+    } while (inputCopy.RunSimulationStep(occupancyTreshold, countFirstSeatInAnyDirection));
+    const auto occupiedCnt = inputCopy.CountSeatsOfState(GridElement::occupiedSeat);
+    std::cout << "In the stable state, " << occupiedCnt << " seats are occupied.\n";
+
     return 0;
 }
 
