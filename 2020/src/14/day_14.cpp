448ad07f8e15c4a428d19555db1661dfe8fa2e24
Pascal Guenther <pascal.develop@pguenther.net>
2021-01-02 14:04:09 +0100

    Solution Day 14 Part 2

--- 2020/src/14/day_14.cpp
+++ 2020/src/14/day_14.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
@@ -20,43 +21,52 @@ namespace
 using std::size_t;
 using std::uint64_t;
 
-constexpr std::string_view aExampleInput[] = {
+constexpr std::string_view aExampleInput1[] = {
     "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
     "mem[8] = 11",
     "mem[7] = 101",
     "mem[8] = 0",
 };
 
-class Computer
+constexpr std::string_view aExampleInput2[] = {
+    "mask = 000000000000000000000000000000X1001X",
+    "mem[42] = 100",
+    "mask = 00000000000000000000000000000000X0XX",
+    "mem[26] = 1",
+};
+
+class Decoder
 {
 public:
     bool ProcessProgramLine(const std::string_view &line)
     {
-        constexpr size_t bitmaskLen = 36;
         constexpr char prefixMask[] = "mask = ";
         constexpr char prefixMem[] = "mem[";
-        if (0 == line.find(prefixMask) && (line.size() == sizeof(prefixMask) - sizeof(static_cast<char>('\0')) + bitmaskLen))
+        if (0 == line.find(prefixMask) && (line.size() == sizeof(prefixMask) - sizeof(static_cast<char>('\0')) + s_bitmaskLen))
         {
             const auto maskSV = line.substr(sizeof(prefixMask) - sizeof(static_cast<char>('\0')));
-            if (bitmaskLen != maskSV.size())
+            if (s_bitmaskLen != maskSV.size())
             {
                 return false;
             }
-            m_maskSet = 0;
-            m_maskReset = 0;
+            m_mask0 = 0u;
+            m_mask1 = 0u;
+            m_maskX = 0u;
             for (const auto &bit : maskSV)
             {
-                m_maskSet <<= 1;
-                m_maskReset <<= 1;
+                m_mask0 <<= 1;
+                m_mask1 <<= 1;
+                m_maskX <<= 1;
                 switch (bit)
                 {
-                    case '1':
-                        m_maskSet |= 1;
-                        break;
                     case '0':
-                        m_maskReset |= 1;
+                        m_mask0 |= 1;
+                        break;
+                    case '1':
+                        m_mask1 |= 1;
                         break;
                     case 'X':
+                        m_maskX |= 1;
                         break;
 
                     default:
@@ -96,14 +106,13 @@ public:
             {
                 return false;
             }
-            value |= m_maskSet;
-            value &= ~m_maskReset;
-
-            m_memory[addr] = value;
+            this->StoreValue(addr, value);
             return true;
         }
         return false;
     }
+
+    virtual void StoreValue(uint64_t addr, uint64_t value) = 0;
 
     [[nodiscard]] uint64_t CalculateMemorySum() const
     {
@@ -112,16 +121,65 @@ public:
                                [](const auto acc, const auto &mem) { return acc + mem.second; });
     }
 
-private:
-    uint64_t m_maskSet{};
-    uint64_t m_maskReset{};
+protected:
+    constexpr static size_t s_bitmaskLen{36u};
+    uint64_t m_mask0{};
+    uint64_t m_mask1{};
+    uint64_t m_maskX{};
     std::map<uint64_t, uint64_t> m_memory;
+};
+class Decoder1 final : public Decoder
+{
+public:
+    void StoreValue(uint64_t addr, uint64_t value) override
+    {
+        value |= m_mask1;
+        value &= ~m_mask0;
+        m_memory[addr] = value;
+    }
+};
+
+class Decoder2 final : public Decoder
+{
+public:
+    void StoreValue(uint64_t addr, uint64_t value) override
+    {
+        addr |= m_mask1;
+        constexpr uint64_t one64Bit{1u};
+        const size_t maxPermutations = one64Bit << AOC::Popcount(m_maskX);
+        for (uint64_t permutation = 0u; permutation < maxPermutations; ++permutation)
+        {
+            uint64_t localMask0{0};
+            uint64_t localMask1{0};
+            size_t bit{0};
+            for (size_t pos = 0; pos < s_bitmaskLen; pos++)
+            {
+                if (m_maskX & (one64Bit << pos))
+                {
+                    if (permutation & (one64Bit << bit))
+                    {
+                        localMask1 |= one64Bit << pos;
+                    }
+                    else
+                    {
+                        localMask0 |= one64Bit << pos;
+                    }
+                    bit++;
+                }
+            }
+            addr |= localMask1;
+            addr &= ~localMask0;
+            m_memory[addr] = value;
+        }
+
+        m_memory[addr] = value;
+    }
 };
 
 int Day14_Part1(const std::vector<std::string> &input)
 {
     std::cout << "=Part 1=\n";
-    Computer computer;
+    Decoder1 computer;
     for (const auto &line : input)
     {
         if (!computer.ProcessProgramLine(line))
@@ -134,10 +192,19 @@ int Day14_Part1(const std::vector<std::string> &input)
     return 0;
 }
 
-int Day14_Part2(const std::vector<std::string> & /* input */)
+int Day14_Part2(const std::vector<std::string> &input)
 {
-    std::cout << "=Part 2=\n";
-
+    std::cout << "=Part 1=\n";
+    Decoder2 computer;
+    for (const auto &line : input)
+    {
+        if (!computer.ProcessProgramLine(line))
+        {
+            return 1;
+        }
+    }
+    const auto memSum = computer.CalculateMemorySum();
+    std::cout << "Sum of all values left in memory: " << memSum << "\n";
     return 0;
 }
 
@@ -151,8 +218,8 @@ int main(const int argc, const char *const argv[])
 #endif
     try
     {
-        const std::vector<std::string> rawInput = [&]() -> std::vector<std::string> {
-            std::vector<std::string> lines;
+        const std::array<std::vector<std::string>, 2> rawInput = [&]() -> std::array<std::vector<std::string>, 2> {
+            std::array<std::vector<std::string>, 2> input;
             if (argc > 1)
             {
                 auto inputVariant = OpenFile(argv[1u]);
@@ -162,24 +229,29 @@ int main(const int argc, const char *const argv[])
                     std::string line;
                     while (std::getline(file, line))
                     {
-                        lines.push_back(line);
+                        input[0].push_back(line);
                     }
-                    return lines;
+                    input[1] = input[0];
+                    return input;
                 }
                 std::cerr << "Failed to open or parse input file";
             }
             std::cout << "Using example input\n";
-            for (const auto &line : aExampleInput)
+            for (const auto &line : aExampleInput1)
             {
-                lines.emplace_back(line);
+                input[0].emplace_back(line);
             }
-            return lines;
+            for (const auto &line : aExampleInput2)
+            {
+                input[1].emplace_back(line);
+            }
+            return input;
         }();
 
-        int ret = Day14_Part1(rawInput);
+        int ret = Day14_Part1(rawInput[0]);
         if (ret == 0)
         {
-            ret = Day14_Part2(rawInput);
+            ret = Day14_Part2(rawInput[1]);
         }
         return ret;
     }
--- 2020/src/common/NumberConversions.hpp
+++ 2020/src/common/NumberConversions.hpp
@@ -3,13 +3,14 @@
 
 #include <cstdint>
 #include <string_view>
+#include <type_traits>
 
 namespace AOC
 {
 
 using std::size_t;
 
-template<typename T>
+template <typename T>
 inline bool ReadFromSv(T &out, const std::string_view &in, const size_t beginPos, const size_t endPos, const int base = 10)
 {
     if (base < 0)
@@ -27,6 +28,21 @@ inline bool ReadFromSv(T &out, const std::string_view &in, const size_t beginPos
         out += in[i] - '0';
     }
     return true;
+}
+
+template <typename T>
+static size_t Popcount(const T num)
+{
+    size_t ret{0};
+    for (size_t i = 0u; i < 8 * sizeof(num); ++i)
+    {
+		constexpr T one{1};
+        if (num & (one << i))
+        {
+            ret++;
+        }
+    }
+    return ret;
 }
 
 } // namespace AOC
